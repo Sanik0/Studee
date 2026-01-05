80ac5d339780fb5b41d33a78989ab1ff939f5d30
Sanik0 <sanico.ronan.macugay@gmail.com>
2026-01-05 20:09:51 +0800

    index form tabs

--- Studee/routes/web.php
+++ Studee/routes/web.php
@@ -3,10 +3,9 @@
 use App\Http\Controllers\QuizController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [QuizController::class, 'index'])->name('quiz.index');
-Route::post('/extract-text', [QuizController::class, 'extractText'])->name('quiz.extract');
-Route::get('/preview', [QuizController::class, 'preview'])->name('quiz.preview');
-
+Route::get('/', function () {
+    return view('index');
+});
 Route::get('/quiz', function () {
     return view('quiz');
 });
