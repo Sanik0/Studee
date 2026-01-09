7da011b1dc6c9e188e072b9936e900db8f77eec5
Sanik0 <sanico.ronan.macugay@gmail.com>
2026-01-09 17:02:05 +0800

    quiz retake changes

--- Studee/resources/views/quiz.blade.php
+++ Studee/resources/views/quiz.blade.php
@@ -34,7 +34,7 @@
                     <div class="z-50 hidden bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-44" id="language-dropdown-menu">
                         <ul class="p-2 text-sm text-body font-medium" role="none">
                             <li>
-                                <a href="#" class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded" role="menuitem">
+                                <a href="{{ route('quiz.show') }}?retake=1" class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded" role="menuitem">
                                     <div class="inline-flex items-center">
                                         <svg class="h-4 w-4 me-1.5 fill-body" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="24px">
                                             <path d="M480-80q-75 0-140.5-28.5t-114-77q-48.5-48.5-77-114T120-440h80q0 117 81.5 198.5T480-160q117 0 198.5-81.5T760-440q0-117-81.5-198.5T480-720h-6l62 62-56 58-160-160 160-160 56 58-62 62h6q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-440q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-80Z" />
